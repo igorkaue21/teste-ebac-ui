229efbed8d0b457637a40f03e3721d83f73d5c40
Igor Kauê <Igorkaue1@hotmail.com>
2025-06-01 05:00:55 -0300

    Automação de testes em Listas

---
+++ cypress/e2e/3-Loja-Ebac/Produtos.cy.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+        .first()
+        .click()
+        cy.get('.single_add_to_cart_button').should('exist')
+        
+    });
+});
